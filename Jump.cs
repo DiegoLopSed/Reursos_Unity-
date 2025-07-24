198812c284869791ea6b3595062982494931374b
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2025-07-24 17:12:45 -0600

    Recursos de unity, saltos

---
+++ Jump.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpForce = 10f;
+    private float move = 0f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        // Revisión de suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        Debug.Log("Está en el suelo: " + isGrounded); // TEMPORAL
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+   
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
+
